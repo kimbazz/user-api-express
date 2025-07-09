e92e951a54a075f4fd8912597dae2d1696df9f43
rezafiansyah <rezafiansyah.putra@gmail.com>
2025-07-09 20:42:34 +0700

    feat: add cors and body parser

--- src/server.js
+++ src/server.js
@@ -3,11 +3,15 @@ import { connectDB } from "./config/database.js";
 import userRoutes from "./interfaces/http/routes/userRoutes.js";
 import errorHandler from "./interfaces/middleware/errorHandler.js";
 import { APP_PORT } from "./config/index.js";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 await connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
 app.use(errorHandler);
 
